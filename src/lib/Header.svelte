08b9ce320714cd6b6029237a962ea253fd3e87f3
Khalid <ga.lid2002.km@gmail.com>
2025-05-05 19:46:11 +0100

    Experimental header

---
+++ src/lib/Header.svelte
@@ -0,0 +1,3 @@
+<nav class="w-80 bg-red-500 text-gray-600 p-8 flex flex-col justify-between">
+
+</nav>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
 	import '../app.css';
+	import Header from '$lib/Header.svelte';
 
 	let { children } = $props();
 </script>
 
-{@render children()}
+<div class="flex min-h-screen">
+	<Header />
+
+	{@render children()}
+</div>
