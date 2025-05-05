2d23b365fcf4ddfdd863f1d2dd4941e8c8bdcff8
Khalid <ga.lid2002.km@gmail.com>
2025-05-05 22:18:04 +0100

    Header logo

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,3 +1,17 @@
-<nav class="w-80 bg-red-500 text-gray-600 p-8 flex flex-col justify-between">
+<script>
+    import logo1 from '$lib/assets/jul (1).png'
+    import logo2 from '$lib/assets/jul (2).png'
+    let currentLogo = $state(logo1)
+</script>
 
+<nav class="w-80 bg-white text-gray-600 p-8 flex flex-col justify-between justify-center items-center">
+    <div>
+        <a href="/">
+            <img alt="julian mosquera"
+            src={currentLogo} 
+            class="w-60"
+            onmouseenter={() => currentLogo = logo2}
+            onmouseleave={() => currentLogo = logo1}/>
+        </a>
+    </div>
 </nav>
