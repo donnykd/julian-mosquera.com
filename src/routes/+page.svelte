18bf0ffffd246a1bee078d7ca3dad1d54d468551
Khalid <ga.lid2002.km@gmail.com>
2025-05-07 20:12:39 +0100

    random image selection

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,15 +11,33 @@
     const verticalImages = [img2, img4, img6]
     const horizontalImages = [img1, img3, img5]
 
-    let currentImage = $state(0)
+    // Pair of horizontal and vertical photos
+    let currentPair = $state({
+        horizontal: horizontalImages[0],
+        vertical: verticalImages [0]
+    })
 
-    function nextImage(){
-        currentImage = (currentImage + 1) % Math.min(verticalImages.length, horizontalImages.length)
+
+    function getRandomImages(){
+        var newHorizontal, newVertical
+        do{
+            newHorizontal = horizontalImages[Math.floor(Math.random() * horizontalImages.length)]
+            newVertical = verticalImages[Math.floor(Math.random() * verticalImages.length)]
+        } while (
+            newHorizontal === currentPair.horizontal && newVertical === currentPair.vertical
+        )
+        return { horizontal: newHorizontal, vertical: newVertical }
     }
+
+    /*function nextImage(){
+        currentImage = (currentImage + 1) % Math.min(verticalImages.length, horizontalImages.length)
+    }*/
 
     // 4 seconds interval
     onMount(() => {
-        const interval = setInterval(nextImage, 4000)
+        const interval = setInterval(() => {
+            currentPair = getRandomImages()
+        }, 4000)
 
         return () => clearInterval(interval)
     })
@@ -29,16 +47,16 @@
 
 <div class="flex justify-center items-center w-full min-h-screen p-4">
     <div class="flex items-center justify-center gap-16 w-6xl">
-        {#key currentImage}
+        {#key currentPair}
         <div in:fade class="max-w-[60%] pr-8">
             <enhanced:img 
-            src={horizontalImages[currentImage]} 
+            src={currentPair.horizontal} 
             alt="landscape shot" />
         </div>
 
         <div in:fade class="max-w-[40%] pl-8">
             <enhanced:img 
-            src={verticalImages[currentImage]} 
+            src={currentPair.vertical} 
             alt="portrait shot" />
         </div>
         {/key}
