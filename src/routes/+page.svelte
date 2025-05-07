3c8f527970ed548976379704ab64a7aa73a38589
Khalid <ga.lid2002.km@gmail.com>
2025-05-07 17:47:31 +0100

    progress on home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,25 @@
 <script>
     import { fade } from 'svelte/transition'
     import { onMount } from 'svelte';
-    import img from '$lib/assets/gallery/DSCF0341_3.jpg?enhanced'
+    import img1 from '$lib/assets/gallery/DSCF0341_3.jpg?enhanced'
     import img2 from '$lib/assets/gallery/DSCF4090.jpg?enhanced'
+    import img3 from '$lib/assets/gallery/DSCF3527 2.jpg?enhanced'
+    import img4 from '$lib/assets/gallery/DSCF4021.jpg?enhanced'
+    import img5 from '$lib/assets/gallery/DSCF0358 3.jpg?enhanced'
+    import img6 from '$lib/assets/gallery/DSCF4107.jpg?enhanced'
 
-    const images = [img, img2]
+    const verticalImages = [img2, img4, img6]
+    const horizontalImages = [img1, img3, img5]
 
     let currentImage = $state(0)
 
     function nextImage(){
-        currentImage = (currentImage + 1) % images.length
+        currentImage = (currentImage + 1) % Math.min(verticalImages.length, horizontalImages.length)
     }
 
+    // 4 seconds interval
     onMount(() => {
-        const interval = setInterval(nextImage, 3000)
+        const interval = setInterval(nextImage, 4000)
 
         return () => clearInterval(interval)
     })
@@ -22,12 +28,18 @@
 
 
 <div class="flex justify-center items-center w-full min-h-screen p-4">
-    <div class="w-xl">
+    <div class="flex items-center justify-center gap-16 w-6xl">
         {#key currentImage}
-        <div in:fade>
+        <div in:fade class="max-w-[60%] pr-8">
             <enhanced:img 
-            src={images[currentImage]} 
-            alt="img" />
+            src={horizontalImages[currentImage]} 
+            alt="landscape shot" />
+        </div>
+
+        <div in:fade class="max-w-[40%] pl-8">
+            <enhanced:img 
+            src={verticalImages[currentImage]} 
+            alt="portrait shot" />
         </div>
         {/key}
     </div>
