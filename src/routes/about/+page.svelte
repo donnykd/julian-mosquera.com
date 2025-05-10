25d5bf99752a80d4e5f24d98f0fdaf1c1bf2a572
Khalid <ga.lid2002.km@gmail.com>
2025-05-10 17:58:37 +0100

    about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,3 +1,12 @@
-<div class="flex justify-center items-center w-full min-h-screen p-4">
-    
+<script>
+    import nira from "$lib/assets/nira.webp?enhanced";
+
+</script>
+
+<div class="flex flex-col justify-center items-center w-full min-h-screen
+            font-[Alegraya Sans] font-mono lowercase tracking-wider text-center text-black">
+    <p>
+        Born in New York — Based in Tokyo — Child of Concrete Jungles
+    </p>
+    <enhanced:img src={nira} alt="Nira Dance" />
 </div>
