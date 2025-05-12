d35a4172d0aac471541211ef96f0c80398a34ac5
Khalid <ga.lid2002.km@gmail.com>
2025-05-12 12:16:19 +0100

    reverted back to no server api

--- src/routes/personal/+page.svelte
+++ src/routes/personal/+page.svelte
@@ -1,7 +1,8 @@
 <script>
-	import { fade } from "svelte/transition";
-
-</script>
-<div in:fade class="flex justify-center items-center w-full min-h-screen p-4">
+    import { fade } from 'svelte/transition'
     
+</script>
+
+<div in:fade class="flex justify-center items-center text-gray-700 w-full min-h-screen p-4">
+
 </div>
