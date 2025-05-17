e45826cfc29fa95c986533cf46f95838d34ef028
Khalid <ga.lid2002.km@gmail.com>
2025-05-17 16:04:26 +0100

    mobile responsive menu

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,8 +1,9 @@
 <script>
-    import logo1 from '$lib/assets/jul (1).png';
-    import logo2 from '$lib/assets/jul (2).png';
+    import logo1 from '$lib/assets/jul (1).png'
+    import logo2 from '$lib/assets/jul (2).png'
+    import { fade } from 'svelte/transition'
     import { receive } from '$lib/transition.js'
-    import { Instagram, Mail, AlignJustify, X } from '@lucide/svelte';
+    import { Instagram, Mail, AlignJustify, X } from '@lucide/svelte'
 
     let currentLogo = $state(logo1);
     let isMenuOpen = $state(false)
@@ -39,6 +40,30 @@
     </div>
 </div>
 
+{#if isMenuOpen}
+    <div 
+    in:fade={{ duration: 100 }}  
+    class="md:hidden fixed inset-0 flex flex-col p-8 bg-white/50 backdrop-blur z-20">
+
+        <ul 
+        class="font-[Alegraya Sans] font-light lowercase tracking-widest space-y-6 text-center text-3xl text-gray-700 w-full">
+            <li><a href="/" class="hover:text-black transition" onclick={() => isMenuOpen = false}>Home</a></li>
+            <li><a href="/personal" class="hover:text-black transition" onclick={() => isMenuOpen = false}>personal</a></li>
+            <li><a href="/projects" class="hover:text-black transition" onclick={() => isMenuOpen = false}>projects</a></li>
+            <li><a href="/about" class="hover:text-black transition" onclick={() => isMenuOpen = false}>about</a></li>
+            <li><a href="/contact" class="hover:text-black transition" onclick={() => isMenuOpen = false}>contact</a></li>
+        </ul>
+
+        <div class="font-[Alegraya Sans] font-light lowercase tracking-wider text-center text-gray-700 mt-auto mr-8">
+            <p class="mb-3">©2025 julianmosquera</p>
+            <div class="flex justify-center space-x-6">
+                <a href="https://www.instagram.com/miramiramemories/" class="hover:text-black transition"><Instagram class="w-5 h-6"/></a>
+                <a href={"mailto:" + email} class="hover:text-black transition"><Mail class="w-5 h-6"/></a>
+            </div>
+        </div>
+
+    </div>
+{/if}
 
 <nav class="hidden md:flex sticky top-0 w-80 h-screen bg-white text-gray-500 p-8 flex-col">
     <div class="flex flex-col justify-center items-center flex-grow space-y-10">
