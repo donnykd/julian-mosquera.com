fab1c5701f58bfd7185edb51739383938f9ea248
khalid <ga.lid2002.km@gmail.com>
2025-06-15 12:58:20 +0100

    fix: home page width issue resolved

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -29,8 +29,8 @@
 	});
 </script>
 
-<div in:fade class="flex min-h-screen w-full items-center justify-center p-4">
-	<div class="flex w-6xl flex-col items-center justify-center gap-8 md:flex-row">
+<div in:fade class="flex min-h-screen w-full items-center justify-center">
+	<div class="flex flex-col items-center justify-center gap-8 md:flex-row">
 		{#key currentImage}
 			<div in:fade class="max-w-[70%] md:max-w-[60%] md:pr-8">
 				<enhanced:img
