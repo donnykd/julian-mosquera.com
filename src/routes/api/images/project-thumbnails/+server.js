4dcce9d2dc12fe9d739792f1b528596551d30d74
khalid <ga.lid2002.km@gmail.com>
2025-06-26 10:34:20 +0100

    fix: issue with finding multiple folders fixed

--- src/routes/api/images/project-thumbnails/+server.js
+++ src/routes/api/images/project-thumbnails/+server.js
@@ -4,16 +4,16 @@ import { json } from '@sveltejs/kit';
 export async function GET() {
 	try {
 		const result = await cloudinary.search
-			.expression('resource_type:image AND folder:Projects/*') //Add cosplays to this later using OR
+			.expression('resource_type:image AND asset_folder:Projects/*') //Add cosplays to this later using OR
 			.with_field('context')
-			.max_results(50)
+			.max_results(100)
 			.execute();
 
 		const seenFolders = new Set();
 		const thumbnails = [];
 
 		for (const image of result.resources) {
-			const folder = image.folder;
+			const folder = image.asset_folder;
 			const isHorizontal = image.width / image.height > 1.45;
 			const [, projectName] = image.asset_folder.split('/');
 			const date = image.context.Date;
